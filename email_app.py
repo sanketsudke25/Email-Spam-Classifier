9e6ac3230cddd8157d39040c4c501ffd9bf6d369
Sanket Sudke <166213828+sanketsudke25@users.noreply.github.com>
2026-02-19 22:51:48 +0530

    email_app.py

--- email_app.py
+++ email_app.py
@@ -1,29 +1,18 @@
 import streamlit as st
 import pickle
 
-# Load saved files
+# Load saved model and vectorizer
 model = pickle.load(open("spam_model.pkl", "rb"))
 vectorizer = pickle.load(open("tfidf_vectorizer.pkl", "rb"))
 
-nlp = spacy.load("en_core_web_sm")
-stop_words = nlp.Defaults.stop_words
-
-st.title("📧 Email Spam Classifier")
+st.title("📧 Email Spam Detector")
 
 text = st.text_area("Enter Email Message")
 
 if st.button("Predict"):
-    words = []
-    doc = nlp(text)
-    
-    for token in doc:
-        if token.lemma_ not in stop_words:
-            words.append(token.lemma_)
-    
-    new_text = " ".join(words)
-    input_data = vectorizer.transform([new_text])
+    input_data = vectorizer.transform([text])
     prediction = model.predict(input_data)
-    
+
     if prediction[0] == "spam":
         st.error("This is Spam Email")
     else:
